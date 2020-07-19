b1bac60cfd4c270b50023e4794ee675706c20a57
Ale Diaz <diazale94@outlook.com>
2020-07-19 00:49:44 -0300

    Mi primer commit

---
+++ Scripts.js
@@ -0,0 +1,14 @@
+alert ("Hola Mundo")
+
+let nombree = 'Alejandro'
+let edadd = 25
+
+let persona = {   
+        nombre : "Alejandro",
+        apellido : "Diaz",
+        edad : 25,
+        ocupacion : 'Estudiante',
+    
+}
+
+alert (`${persona.nombre} ${persona.apellido}`)
